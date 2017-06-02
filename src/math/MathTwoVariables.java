f8fdfbfd1a0a498d032f69f8fdd3f6992acf1f84
student <avrildouglas>
2017-06-02 17:44:23 -0400

    first commit

---
+++ src/math/MathTwoVariables.java
@@ -0,0 +1,14 @@
+package math;
+
+public class MathTwoVariables {
+	 public static void main(String[] args) {
+	 double myothernumber;
+	 int mynumber;
+	 
+	 mynumber = 2;
+	 myothernumber = 1.7938;
+	 
+	 System.out.println(mynumber);
+	 System.out.println(myothernumber);
+	 }
+	}	
